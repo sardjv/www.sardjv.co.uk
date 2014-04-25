fca2e99276316210e290534c1e9ee468fdc49d52
Kevin Monk <kevin@sardjv.co.uk>
2014-04-25 16:10:24 +0100

    Second navbar should narrow when you start scrolling down the page.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
+'use strict';
+
 //console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+    $(this).scroll(function(){
+        $('.navbar2').toggleClass('narrow', $(this).scrollTop() > 90);
+    });
+});
