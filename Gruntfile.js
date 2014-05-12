757dfe76fbb5d66f96d24b31659989a8f35b21eb
Kevin Monk <kevin@sardjv.co.uk>
2014-05-12 16:50:29 +0100

    Change where bowerInstall looks

--- Gruntfile.js
+++ Gruntfile.js
@@ -252,7 +252,7 @@ module.exports = function (grunt) {
         // Automatically inject Bower components into the HTML file
         bowerInstall: {
             app: {
-                src: ['<%= config.app %>/index.html'],
+                src: ['<%= config.jekyll %>/_includes/footer.html'],
                 exclude: ['bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap.js']
             },
             sass: {
