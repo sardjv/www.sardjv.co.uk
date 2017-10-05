3973cd882f04bbbdf4696f0b82a8f640384a3c05
Kevin Monk <kevin@sardjv.co.uk>
2017-10-05 17:20:19 +0100

    Dockerized it! Removed all the grunt, bower and other fluff. Simple jekyll site now.

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 30) {
+            $('#navbar2 .desktop').addClass('hidden-sm hidden-md hidden-lg');
+            $('#navbar2 .mobile').addClass('visible-sm visible-md visible-lg');
+        } else {
+            $('#navbar2 .desktop').removeClass('hidden-sm hidden-md hidden-lg');
+            $('#navbar2 .mobile').removeClass('visible-sm visible-md visible-lg');
+        }
+    });
+
+    var $grid = $('.grid').isotope({
+        itemSelector: '.grid-item',
+        masonry: {
+            columnWidth: '.grid-sizer',
+            percentPosition: true
+        }
+    });
+
+    $grid.imagesLoaded().progress( function() {
+        $grid.isotope('layout');
+    });
+
+});
+
